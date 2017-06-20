ced20fae0d3a2698e7f7ff9f78052ac4366bb85e
RGBboy <l-_-l@rgbboy.com>
2017-06-20 20:42:47 +0100

    Fix casing;

--- src/Pages.elm
+++ src/Pages.elm
@@ -57,27 +57,27 @@ collages =
         "/images/lines-and-other-shapes/two-blocks.jpg"
     , C.work
         "Blue Lines"
-        "Ink on Paper"
+        "Ink on paper"
         "100 x 100 mm"
         "/images/lines-and-other-shapes/blue-lines.jpg"
     , C.work
         "Black, Grey and Orange"
-        "Felt Tip Pen on Paper"
+        "Felt tip pen on paper"
         "210 x 297 mm"
         "/images/lines-and-other-shapes/black-grey-and-orange.jpg"
     , C.work
         "Black Lines"
-        "Ink on Paper"
+        "Ink on paper"
         "297 x 210 mm"
         "/images/lines-and-other-shapes/black-lines.jpg"
     , C.work
         "Untitled 29"
-        "Mixed Media on Paper"
+        "Mixed media on paper"
         "290 x 385 mm"
         "/images/lines-and-other-shapes/untitled-29.jpg"
     , C.work
         "Tally Marks"
-        "Pen and Pencil on Paper"
+        "Pen and pencil on paper"
         "297 x 210 mm"
         "/images/lines-and-other-shapes/tally-marks.jpg"
     , C.scrollToTop "collages"
